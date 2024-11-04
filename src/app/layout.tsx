1fc9d18b1b670a959648a4ff2aad7547febd33f5
mark1996 <diaryads9@gmail.com>
2024-11-04 17:43:05 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 // src/app/layout.tsx
 "use client";
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
 export default function RootLayout({
