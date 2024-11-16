010b22809f75b8fd4516bf29a2a1175c5550c722
mark1996 <diaryads9@gmail.com>
2024-11-16 15:15:27 +0700

    update

--- scripts/env.mjs
+++ scripts/env.mjs
@@ -6,8 +6,8 @@ const { TIDB_USER, TIDB_PASSWORD, TIDB_HOST, TIDB_PORT, TIDB_DB_NAME = 'bookshop
 // Notice: When using TiDb Cloud Serverless Tier, you **MUST** set the following flags to enable tls connection.
 const SSL_FLAGS = 'pool_timeout=60&sslaccept=accept_invalid_certs';
 
-if(TIDB_USER && TIDB_HOST && TIDB_PORT) {
-    console.log(`mysql://${TIDB_USER}:${TIDB_PASSWORD}@${TIDB_HOST}:${TIDB_PORT}/${TIDB_DB_NAME}?${SSL_FLAGS}`);
-} else {
-    console.log(`${DATABASE_URL}?${SSL_FLAGS}`);
-}
+// if(TIDB_USER && TIDB_HOST && TIDB_PORT) {
+//     console.log(`mysql://${TIDB_USER}:${TIDB_PASSWORD}@${TIDB_HOST}:${TIDB_PORT}/${TIDB_DB_NAME}?${SSL_FLAGS}`);
+// } else {
+//     console.log(`${DATABASE_URL}?${SSL_FLAGS}`);
+// }
