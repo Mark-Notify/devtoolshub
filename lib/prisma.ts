6e1958624e8fcc5c35e708f7d90d946b2ead83b1
mark1996 <diaryads9@gmail.com>
2024-11-16 15:24:27 +0700

    update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -19,34 +19,34 @@ let prisma: PrismaClient
 
 const { TIDB_USER, TIDB_PASSWORD, TIDB_HOST, TIDB_PORT, TIDB_DB_NAME = 'bookshop', DATABASE_URL } = process.env;
 // Notice: When using TiDb Cloud Serverless Tier, you **MUST** set the following flags to enable tls connection.
-// const SSL_FLAGS = 'pool_timeout=60&sslaccept=accept_invalid_certs';
-// const databaseURL = DATABASE_URL
-//     ? `${DATABASE_URL}?${SSL_FLAGS}`
-//     : `mysql://${TIDB_USER}:${TIDB_PASSWORD}@${TIDB_HOST}:${TIDB_PORT}/${TIDB_DB_NAME}?${SSL_FLAGS}`;
+const SSL_FLAGS = 'pool_timeout=60&sslaccept=accept_invalid_certs';
+const databaseURL = DATABASE_URL
+    ? `${DATABASE_URL}?${SSL_FLAGS}`
+    : `mysql://${TIDB_USER}:${TIDB_PASSWORD}@${TIDB_HOST}:${TIDB_PORT}/${TIDB_DB_NAME}?${SSL_FLAGS}`;
 
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient({
-//     datasources: {
-//       db: {
-//         url: databaseURL,
-//       },
-//     },
-//   });
-// } else {
-  // if (!global.prisma) {
-  //   global.prisma = new PrismaClient({
-  //     datasources: {
-  //       db: {
-  //         url: databaseURL,
-  //       },
-  //     },
-  //   });
-  // }
-  // prisma = global.prisma
-// }
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: databaseURL,
+      },
+    },
+  });
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient({
+      datasources: {
+        db: {
+          url: databaseURL,
+        },
+      },
+    });
+  }
+  prisma = global.prisma
+}
 
-// BigInt.prototype.toJSON = function() {       
-//   return this.toString()
-// }
+BigInt.prototype.toJSON = function() {       
+  return this.toString()
+}
 
 export default prisma
