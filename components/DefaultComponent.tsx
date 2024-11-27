5b56af2a93f8a57e6bdf5028edc306488ec6413d
Mark <diaryads9@gmail.com>
2024-11-27 10:57:59 +0700

    Update DefaultComponent.tsx

--- components/DefaultComponent.tsx
+++ components/DefaultComponent.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const ComponentA: React.FC = () => {
   return (
-    <div className="p-4 border rounded-md shadow-md">
+    <div className="p-4 border rounded-md shadow-md min-h-screen">
       <h2 className="text-xl font-bold">Component A</h2>
       <p>This is Component A. You can put content here.</p>
     </div>
