76a308e05d5c8dcafabf612bbbdb2007381b5212
mark1996 <diaryads9@gmail.com>
2024-12-03 14:30:51 +0700

    update config

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useState } from "react";
 import Header, { HeaderProps } from "components/Layout/Header";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from "@vercel/analytics/next";
 
 export interface CommonLayoutProps {
   children?: any;
@@ -23,33 +23,41 @@ export default function CommonLayout(props: CommonLayoutProps) {
     }
   };
 
+  // ฟังก์ชันย่อยสำหรับ Analytics
+  const RenderAnalytics = () => {
+    return process.env.NODE_ENV === "production" ? <Analytics /> : null;
+  };
+
+  // ฟังก์ชันย่อยสำหรับ SpeedInsights
+  const RenderSpeedInsights = () => {
+    return process.env.NODE_ENV === "production" ? <SpeedInsights /> : null;
+  };
+
   return (
     <div className="flex flex-col min-h-screen" style={{ fontFamily: "Courier Prime, serif" }}>
       <Header {...headerProps} onThemeChange={handleThemeChange} />
       <main>
+        <RenderAnalytics />
         <div key={refreshKey} className="">
-          {/* การเพิ่ม key ให้กับ children เพื่อให้ React รีเรนเดอร์ใหม่ทุกครั้งที่ refreshKey เปลี่ยน */}
           {children}
-          <Analytics />
-          <SpeedInsights />
         </div>
-        <footer className="p-4 shadow bg-base-100">
-          <div className="text-center">
-            <p className="text-lg">
-              &copy; {new Date().getFullYear()} Dev Tools. All rights reserved.
-              |{" "}
-              <a
-                href="https://devtools2.vercel.app"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline hover:text-blue-500"
-              >
-                devtools.app
-              </a>
-            </p>
-          </div>
-        </footer>
       </main>
+      <footer className="p-4 shadow bg-base-100">
+        <div className="text-center">
+          <p className="text-lg">
+            &copy; {new Date().getFullYear()} Dev Tools. All rights reserved. |{" "}
+            <a
+              href="https://devtools2.vercel.app"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline hover:text-blue-500"
+            >
+              devtools.app
+            </a>
+          </p>
+        </div>
+        <RenderSpeedInsights />
+      </footer>
     </div>
   );
 }
