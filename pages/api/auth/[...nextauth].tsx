34e34cd91a3b2b2fbb0c52862c192fc83ed9488c
mark1996 <diaryads9@gmail.com>
2025-08-10 16:47:17 +0700

    fixed bug

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -1,7 +1,5 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { connectToDatabase } from "../../../lib/mongodb";
-import UserData from "../../../models/UserData";
 
 export default NextAuth({
   providers: [
