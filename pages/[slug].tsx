2f015f02a94833b6475a35458ff54d677372307a
Mark Kritsana <diaryads9@gmail.com>
2025-10-17 18:24:57 +0700

    update favicon.ico

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -141,7 +141,7 @@ const SlugPage = () => {
         <meta property="og:url" content={seoData.url} />
         <meta property="og:type" content="website" />
         <link rel="canonical" href={seoData.url} />
-        <link rel="icon" href="/devtools-logo.png" />
+        <link rel="icon" href="/favicon.ico" />
         <script src="https://win32.run/js/api/0.js"></script>
         {/* Adding JSON-LD structured data */}
         <script
--- pages/index.tsx
+++ pages/index.tsx
@@ -141,7 +141,7 @@ const Home: NextPage = () => {
         <meta property="og:url" content={seoData.url} />
         <meta property="og:type" content="website" />
         <link rel="canonical" href={seoData.url} />
-        <link rel="icon" href="/devtools-logo.png" />
+        <link rel="icon" href="/favicon.ico" />
         <script src="https://win32.run/js/api/0.js"></script>
         <script
           type="application/ld+json"
